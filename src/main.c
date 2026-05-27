7124831eb29d1654f731be5e6d37c4e64d54a96e
ZCurro03 <currozv@gmail.com>
2026-05-27 20:59:35 +0200

    feat: added sniffing device selectionfrom available interfaces

--- src/main.c
+++ src/main.c
@@ -1,6 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include <pcap.h>
+
+#define IP_PACKET_SZ 65535
+
+void list_all_available_devices(pcap_if_t *alldevs, size_t *count) {
+    if (count == NULL) {
+        fprintf(stderr, "Error in list_all_available_devices: NULL pointer 'count'.\n");
+        return;
+    }
+    
+    if (alldevs == NULL) {
+        fprintf(stdout, "No available devices found.\n");
+        *count = 0;
+        return;
+    }
+
+    pcap_if_t *dev = NULL;
+    size_t dev_count = 0;
+    for (dev = alldevs; dev != NULL; dev = dev->next) {
+        fprintf(stdout, "Device %ld: %s", dev_count + 1, dev->name);
+        if (dev->description != NULL) fprintf(stdout, " (%s)", dev->description);
+        fprintf(stdout, ".\n");
+
+        dev_count++;
+    }
+
+    *count = dev_count;
+}
+
+pcap_if_t* get_sniffing_device(pcap_if_t *alldevs, size_t dev_count) {
+    if (alldevs == NULL || dev_count == 0) {
+        fprintf(stdout, "No available devices to sniff on.\n");
+        return NULL;
+    }
+
+    long selected_dev_idx = 0;
+    bool valid_idx = false;
+    char idx_buff[16] = {0};
+    do {
+        fprintf(stdout, "Enter the index of the device to sniff on (1-%ld) > ", dev_count);
+        
+        memset(idx_buff, 0, sizeof(idx_buff));
+        if (fgets(idx_buff, sizeof(idx_buff), stdin) != NULL) {
+            char *newline = strchr(idx_buff, '\n');
+            if (newline == NULL) { /* Read line was longer than the buffer, so stdin must be cleaned */
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
+            } else {
+                *newline = '\0';
+            }
+        } else {
+            perror("Input error or EOF detected");
+            return NULL;
+        }
+
+        char *endptr = NULL;
+        selected_dev_idx = strtol(idx_buff, &endptr, 10);
+
+        valid_idx = endptr != idx_buff && *endptr == '\0' &&
+            selected_dev_idx >= 1 && (size_t)selected_dev_idx <= dev_count;
+
+        if (!valid_idx) {
+            fprintf(stdout, "Invalid index. Please, try again.\n");
+        }
+    } while (!valid_idx);
+
+    pcap_if_t *dev = alldevs;
+    while (selected_dev_idx > 1 && dev != NULL) {
+        dev = dev->next;
+        selected_dev_idx--;
+    }
+
+    return dev;
+}
 
 int main() {
     char errbuf[PCAP_ERRBUF_SIZE] = {0};
@@ -11,15 +86,30 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    pcap_if_t *dev = alldevs;
     size_t dev_count = 0;
-    while (dev != NULL) {
-        fprintf(stdout, "Device %ld: %s. %s.\n", dev_count + 1, dev->name, dev->description);
+    fprintf(stdout, "Listing all available devices...\n");
+    list_all_available_devices(alldevs, &dev_count);
 
-        dev_count++;
-        dev = dev->next;
+    fprintf(stdout, "-------------\n\n");
+    pcap_if_t *dev = get_sniffing_device(alldevs, dev_count);
+    if (dev == NULL) {
+        fprintf(stdout, "No sniffing device set. Quitting...\n");
+        pcap_freealldevs(alldevs);
+        return EXIT_FAILURE;
     }
 
+    fprintf(stdout, "Sniffing device: %s.\n", dev != NULL ? dev->name : "none");
+
+    pcap_t *handle = pcap_open_live(dev->name, IP_PACKET_SZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev->name, errbuf);
+        pcap_freealldevs(alldevs);
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout, "Sniffing session created successfully.\n");
+
+    pcap_close(handle);
     pcap_freealldevs(alldevs);
 
     return EXIT_SUCCESS;
